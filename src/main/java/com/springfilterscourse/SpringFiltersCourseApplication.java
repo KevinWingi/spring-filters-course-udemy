27f3e375d4cdb960766b1265de59a3b9325f4cfb
KevinWingi <kevinwing8@gmail.com>
2021-04-22 22:54:50 +0200

    Initial commit

---
+++ src/main/java/com/springfilterscourse/SpringFiltersCourseApplication.java
@@ -0,0 +1,13 @@
+package com.springfilterscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFiltersCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFiltersCourseApplication.class, args);
+	}
+
+}

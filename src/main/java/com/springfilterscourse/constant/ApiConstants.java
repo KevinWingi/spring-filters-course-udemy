31d095f318719c4dcaae8a9b3024cb4ce8de3dd1
KevinWingi <kevinwing8@gmail.com>
2021-06-10 21:31:45 +0200

    Implement sort

--- src/main/java/com/springfilterscourse/constant/ApiConstants.java
+++ src/main/java/com/springfilterscourse/constant/ApiConstants.java
@@ -6,5 +6,7 @@ public class ApiConstants {
 	public static final String PAGE_KEY = "page";
 	public static final Integer DEFAULT_LIMIT = 10;
 	public static final Integer DEFAULT_PAGE = 0;
+	public static final String SORT_KEY = "sort";
+	public static final String DEFAULT_SORT = "";
 	
 }
--- src/main/java/com/springfilterscourse/model/FilterModel.java
+++ src/main/java/com/springfilterscourse/model/FilterModel.java
@@ -1,9 +1,13 @@
 package com.springfilterscourse.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 
 import static com.springfilterscourse.constant.ApiConstants.*;
 
@@ -13,14 +17,42 @@ import lombok.Getter;
 public class FilterModel {
 	private Integer limit;
 	private Integer page;
-	
+	private String sort;
+
 	public FilterModel(Map<String, String> params) {
 		this.limit = params.containsKey(LIMIT_KEY) ? Integer.valueOf(params.get(LIMIT_KEY)) : DEFAULT_LIMIT;
 		this.page = params.containsKey(PAGE_KEY) ? Integer.valueOf(params.get(PAGE_KEY)) : DEFAULT_PAGE;
+		this.sort = params.containsKey(SORT_KEY) ? params.get(SORT_KEY) : DEFAULT_SORT;
 	}
-	
+
 	public Pageable toSpringPegeable() {
-		return PageRequest.of(page, limit);
+		List<Order> orders = getOrders();
+
+		return PageRequest.of(page, limit, Sort.by(orders));
 	}
- 	
+
+	private List<Order> getOrders() {
+		List<Order> orders = new ArrayList<>();
+
+		String[] properties = sort.split(",");
+
+		for (String property : properties) {
+
+			if (!property.trim().isEmpty()) {
+				String column = "";
+
+				if (property.startsWith("-")) {
+					column = property.replace("-", "").trim();
+					orders.add(Order.desc(column));
+				} else {
+					column = property.trim();
+					orders.add(Order.asc(column));
+				}
+
+			}
+		}
+		
+		return orders;
+	}
+
 }

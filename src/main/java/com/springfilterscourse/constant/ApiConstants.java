18f88076cbda78850d9c6a74d9c17fbc74bc2184
KevinWingi <KevinWing8@gmail.com>
2024-02-19 21:10:17 +0200

    Date Filter - Add date filter param elements and date format

--- src/main/java/com/springfilterscourse/constant/ApiConstants.java
+++ src/main/java/com/springfilterscourse/constant/ApiConstants.java
@@ -12,7 +12,11 @@ public class ApiConstants {
 	public static final String DEFAULT_EQUAL_FILTERS = null;
 	public static final String IN_FILTERS_KEY = "in_filters";
 	public static final String DEFAULT_IN_FILTERS = null;
+	public static final String DATE_FILTERS_KEY ="date_filters";
+	public static final String DEFAULT_DATE_FILTERS_KEY = null;
 	
 	public static final String DOT = ".";
+	
+	public static final String DATE_FORMAT = "yyyy-MM-dd";
 	
 }
